fee52f171d640aa2eff25346bcf7b8c9de06bbb1
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-06-02 20:09:29 -0600

    comment time and space complexity values

--- example3.js
+++ example3.js
@@ -1,4 +1,6 @@
 
+// O(n^2) time complexity
+// O(1) space complexity
 // exports.commonItem = function(array1, array2){
 //   for(let i=0; i< array1.length; i++){
 //     for(let j=0; j< array2.length; j++){
@@ -10,7 +12,8 @@
 //   return false;
 // }
 
-
+// O(n) time complexity
+// O(a) space complexity
 exports.commonItem2 = function(array1, array2){
 // loop through first array and create items as properties
 
