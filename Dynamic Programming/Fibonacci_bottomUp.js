6dbdd6829f55d7dee8c19082f6b78621e2e585f9
Venkatesh Chandra <54965123+chandravenky@users.noreply.github.com>
2021-07-04 08:37:41 -0600

    Fibonacci - bottom up approach

---
+++ Dynamic Programming/Fibonacci_bottomUp.js
@@ -0,0 +1,10 @@
+function fibonnacibottomup(n){
+  let answer = [0,1];
+  for(let i=2; i<=n; i++){
+    answer.push(answer[i-2] + answer[i-1])
+  }
+  return answer.pop();
+}
+
+
+fibonnacibottomup(30)
